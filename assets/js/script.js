4f92e818b2d36b57a8aa50113504eef805a931d0
nadsbuhdha <nadsbuhdha@gmail.com>
2021-11-27 13:18:37 +0000

    end game feedback message added

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,7 @@ const questionDisplay = document.getElementById ('qcounter');
 window.onload = function scoreFeedback() {
 const finalScores = document.getElementById('player-score');
 const myScore = localStorage.getItem('finalScore')
+const feedback = document.getElementById('feedback')
 finalScores.innerText = myScore
 
 //  if statement to change colour of users final score 
@@ -16,7 +17,21 @@ if (myScore <= 0 ) {
 else {
     finalScores.style.color = 'green'
 }
+
+// if statement to display feedback message on score
+if (myScore >= 10 ) {
+    feedback.innerHTML = "Well done! Looks like you've been revising!"
 }
+else if (myScore >= 1 ) {
+    feedback.innerHTML = "Not bad! Keep revising"
+}
+
+else if (myScore <= 0) {
+    feedback.innerHTML = "I think you need to do a little bit more revising!"
+}
+
+}
+
 
 
 
